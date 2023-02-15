905e0f71f44b629f9e4882231c39e33252da90f7
CODE-ZAX <86313946+CODE-ZAX@users.noreply.github.com>
2023-02-15 19:30:31 +0500

    Setting up Client

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_desBa9rbsTEUv36LnlvKn8XZZNrUL7s",
+  authDomain: "dusecachatmodule.firebaseapp.com",
+  projectId: "dusecachatmodule",
+  storageBucket: "dusecachatmodule.appspot.com",
+  messagingSenderId: "115366637774",
+  appId: "1:115366637774:web:607de8458d0ecb92f7e09f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
